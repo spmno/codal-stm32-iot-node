09225d8056ef1e01df6da75ef162ddc75b2f94bb
Paul ADAM <paul.adam@inidinn.com>
2018-02-12 17:27:01 +0100

    try mbed code

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,27 +1,39 @@
-#include "STM32IotNode.h"
+#include "mbed.h"
 
-STM32IotNode IotNode;
+DigitalOut led1(LED1);
 
-void
-onClick(Event)
-{
-    IotNode.serial.printf("CLICK\r\n");
-}
-
-int
-main()
-{
-    IotNode.init();
-    IotNode.serial.printf(" *** STM32_IOT_NODE BLINKY TEST ***\r\n");
-
-    IotNode.messageBus.listen(DEVICE_ID_BUTTON_A, DEVICE_BUTTON_EVT_CLICK, onClick);
-
-    while(1)
-    {
-        IotNode.io.led.setDigitalValue(1);
-        IotNode.sleep(200);
-
-        IotNode.io.led.setDigitalValue(0);
-        IotNode.sleep(200);
+// main() runs in its own thread in the OS
+int main() {
+    while (true) {
+        led1 = !led1;
+        wait(0.2);
     }
 }
+//
+//#include "STM32IotNode.h"
+//
+//STM32IotNode IotNode;
+//
+//void
+//onClick(Event)
+//{
+//    IotNode.serial.printf("CLICK\r\n");
+//}
+//
+//int
+//main()
+//{
+//    IotNode.init();
+//    IotNode.serial.printf(" *** STM32_IOT_NODE BLINKY TEST ***\r\n");
+//
+//    IotNode.messageBus.listen(DEVICE_ID_BUTTON_A, DEVICE_BUTTON_EVT_CLICK, onClick);
+//
+//    while(1)
+//    {
+//        IotNode.io.led.setDigitalValue(1);
+//        IotNode.sleep(200);
+//
+//        IotNode.io.led.setDigitalValue(0);
+//        IotNode.sleep(200);
+//    }
+//}
