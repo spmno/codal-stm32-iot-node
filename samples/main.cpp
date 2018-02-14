1db216f65ab1f54006bec2f11da2f54f9a147368
Paul ADAM <paul.adam@inidinn.com>
2018-02-14 11:15:39 +0100

    test

--- samples/main.cpp
+++ samples/main.cpp
@@ -32,9 +32,9 @@ main()
     while(1)
     {
         IotNode.io.led.setDigitalValue(1);
-        IotNode.sleep(1000);
+        IotNode.sleep(10);
 
         IotNode.io.led.setDigitalValue(0);
-        IotNode.sleep(1000);
+        IotNode.sleep(10);
     }
 }
