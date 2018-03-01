e150a7a1ac53e888659e430f632585ff606fc87b
Paul ADAM <paul.adam@inidinn.com>
2018-03-01 17:23:21 +0100

    commit for compile

--- model/STM32IotNode.cpp
+++ model/STM32IotNode.cpp
@@ -113,8 +113,10 @@ uint8_t Sensor_IO_Write(void *handle, uint8_t WriteAddr, uint8_t *pBuffer, uint1
 	device_instance->serial.printf( " handle          = %08X\n", handle );
 	device_instance->serial.printf( " handle->address = %08X\n", ( ( DrvContextTypeDef* ) handle )->address );
 	device_instance->serial.printf( " WriteAddr       = %02X\n", WriteAddr );
-	device_instance->serial.printf( " pBuffer         = %08X\n", pBuffer );
 	device_instance->serial.printf( " nBytesToWrite   = %04X\n", nBytesToWrite );
+	device_instance->serial.printf( " pBuffer         = %08X\n", pBuffer );
+	for ( int i = 0; i < nBytesToWrite; i ++ )
+		device_instance->serial.printf( "  pBuffer[ %-2d ] = %02X\n", i, pBuffer[ i ] );
     if ( nBytesToWrite == 1 )
 		device_instance->i2c.write( ( ( DrvContextTypeDef* ) handle )->address, WriteAddr, *pBuffer );
 	return 0;
@@ -126,9 +128,11 @@ uint8_t Sensor_IO_Read(void *handle, uint8_t ReadAddr, uint8_t *pBuffer, uint16_
 	device_instance->serial.printf( " handle          = %08X\n", handle );
 	device_instance->serial.printf( " handle->address = %08X\n", ( ( DrvContextTypeDef* ) handle )->address );
 	device_instance->serial.printf( " ReadAddr        = %02X\n", ReadAddr );
-	device_instance->serial.printf( " pBuffer         = %08X\n", pBuffer );
 	device_instance->serial.printf( " nBytesToRead    = %04X\n", nBytesToRead );
+	device_instance->serial.printf( " pBuffer         = %08X\n", pBuffer );
 	device_instance->i2c.read( ( ( DrvContextTypeDef* ) handle )->address, ReadAddr | 0x1, pBuffer, nBytesToRead);
+	for ( int i = 0; i < nBytesToRead; i ++ )
+		device_instance->serial.printf( "  pBuffer[ %-2d ] = %02X\n", i, pBuffer[ i ] );
 	return 0;
 }
 
