f9d57c317c38df939d06ec50fdeb4bf03268021c
Paul ADAM <paul.adam@inidinn.com>
2018-04-13 11:14:18 +0200

    commit for compile

--- model/STM32IotNodeTemperature.cpp
+++ model/STM32IotNodeTemperature.cpp
@@ -95,10 +95,10 @@ int STM32IotNodeTemperature::requestUpdate()
  if ( ( ( TEMPERATURE_Drv_t* ) DrvContext.pVTable )->Get_Temp( &DrvContext, &Data ) == COMPONENT_OK )
  {
   sample = ( uint16_t ) ( Data * 10.0 );
-  sample = 12;
+  if ( Data > 15 )
+   sample = 12;
   return DEVICE_OK;
  }
- sample = 15;
  return DEVICE_I2C_ERROR;
 }
 
