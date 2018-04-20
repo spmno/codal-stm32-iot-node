0ec51d6fcfbe2632389d822c10a89dfe97d48e79
Paul ADAM <paul.adam@inidinn.com>
2018-04-20 10:56:30 +0200

    commit for compile

--- samples/main.cpp
+++ samples/main.cpp
@@ -40,7 +40,7 @@ main()
  {
 //  int16_t SamplePressure    = IotNode.pressure.getSample( );
 //  IotNode.serial.printf("%d\n", SamplePressure );
-  int16_t SampleHumidity    = IotNode.humidity.getSample( );
+  int16_t SampleHumidity    = IotNode.humidity.getValue( );
   int16_t SampleTemperature = IotNode.temperature.getSample( );
   IotNode.serial.printf("%d, %d\n", SampleHumidity, SampleTemperature );
 //  codal::Sample3D Sample = IotNode.gyroscope.getSample( );
