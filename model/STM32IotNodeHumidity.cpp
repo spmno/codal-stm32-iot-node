10ba3c2dc7155da3e2abdb8b2820986954da5706
Paul ADAM <paul.adam@inidinn.com>
2018-04-20 10:55:35 +0200

    commit for compile

--- model/STM32IotNodeHumidity.cpp
+++ model/STM32IotNodeHumidity.cpp
@@ -95,7 +95,7 @@ int STM32IotNodeHumidity::readValue()
  float Data;
  if ( ( ( HUMIDITY_Drv_t* ) DrvContext.pVTable )->Get_Hum( &DrvContext, &Data ) == COMPONENT_OK )
  {
-  return = ( int ) ( Data * 10.0 );
+  return ( int ) ( Data * 10.0 );
  }
  return DEVICE_I2C_ERROR;
 }
