501498fc8690b3cc75d786a5c007a4a450193762
Sébastien NEDJAR <nedseb@goldaire.com>
2018-07-02 11:12:37 +0200

    Modify sample to use fiber_sleep in place of MBed Sleep

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,7 +1,6 @@
-
 #include "STM32IotNode.h"
-
 using namespace codal;
+
 STM32IotNode IotNode;
 
 int main()
@@ -9,11 +8,12 @@ int main()
     IotNode.init();
     IotNode.serial.printf("*** STM32_IOT_NODE BLINKY TEST ***\r\n");
 
-    while (1)
+    int state = 0;
+
+    while(1)
     {
-        IotNode.io.led.setDigitalValue(1);
-        IotNode.sleep(2000);
-        IotNode.io.led.setDigitalValue(0);
-        IotNode.sleep(1000);
+        IotNode.io.led.setDigitalValue(state);
+        fiber_sleep(1000);
+        state = !state;
     }
 }
