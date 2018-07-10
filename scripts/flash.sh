56fe8a88ed2f686ea870d6cab0e34678e3dd7829
Sébastien NEDJAR <nedseb@goldaire.com>
2018-07-10 14:45:03 +0200

    update debug script

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 ./build.py
-openocd -s "/usr/share/openocd/scripts/" -s"libraries/codal-stm32-iot-node/scripts/" -f "openocd.cfg" -c "program build/STM32_IOT_NODE.hex verify reset exit "
+openocd -s "/usr/share/openocd/scripts/" -s"libraries/codal-stm32-iot-node/scripts/" -f "openocd.cfg" -c "program STM32_IOT_NODE.hex verify reset exit "
