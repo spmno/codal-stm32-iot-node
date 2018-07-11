0f1c725cd626f0ee60a260be330d8a88372f7a0c
Sébastien NEDJAR <nedseb@goldaire.com>
2018-07-11 11:15:40 +0200

    update example

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,21 +1,18 @@
 #include "STM32IotNode.h"
-using namespace codal;
+#include "stm32.h"
 
+using namespace codal;
 STM32IotNode IotNode;
 
 int main()
 {
-    IotNode.io.led2.setDigitalValue(true);
     IotNode.init();
-    //IotNode.serial.printf("*** STM32_IOT_NODE BLINKY TEST ***\r\n");
-
     int state = 0;
-
     while(1)
     {
         IotNode.io.led.setDigitalValue(state);
-        IotNode.io.led2.setDigitalValue(state);
-        fiber_sleep(1000);
+        IotNode.io.led2.setDigitalValue(!state);
+        IotNode.sleep(1000);
         state = !state;
     }
 }
