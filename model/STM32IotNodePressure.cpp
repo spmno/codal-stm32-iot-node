fbda5adb8b9455a43acd971e87afa111d521f44e
Sébastien NEDJAR <nedseb@goldaire.com>
2018-11-02 11:04:18 +0200

    Update pressure sensor for corresponding to the new BSP

--- model/STM32IotNodePressure.cpp
+++ model/STM32IotNodePressure.cpp
@@ -1,96 +1,127 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2016 Lancaster University, UK.
-
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
-*/
-
 /**
-  * Class definition for pressure.
-  * Represents the pressure on the STM IOT node.
-  */
+ * Class definition for pressure.
+ * Represents the pressure on the STM IOT node.
+ */
+
+#include "CodalConfig.h"
 #include "STM32IotNode.h"
 #include "STM32IotNodePressure.h"
 
 namespace codal
 {
-
-/**
-  * Constructor.
-  *
-  * Create a representation of the pressure on the STM32 IOT node
-  *
-  */
-STM32IotNodePressure::STM32IotNodePressure( STM32L4xxI2C& i2c )
-:  codal::Sensor(DEVICE_ID_PRESSURE)
-, _i2c( i2c )
-{
+  /**
+   * Constructor.
+   *
+   * Create a representation of the pressure on the STM32 IOT node
+   *
+   */
+  STM32IotNodePressure::STM32IotNodePressure( STM32L4xxI2C& i2c )
+  :  Sensor(DEVICE_ID_HUMIDITY), 
+    _i2c( i2c ),
+    psensor_drv(&LPS22HB_P_Drv),
+    isInitialized(false)
+  {
+    configure();
     updateSample( );
-}
+  }
 
-/**
- * Configures the pressure range defined
- * in this object. The nearest values are chosen to those defined
- * that are supported by the hardware. The instance variables are then
- * updated to reflect reality.
- *
- * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the pressure could not be configured.
- *
- */
+  /**
+   * Configures output data rate register
+   *
+   *
+   */
+  void updateODR(uint8_t odr){
+    uint8_t tmp;
+    /* Read CTRL_REG1 */
+    tmp = SENSOR_IO_Read(LPS22HB_I2C_ADDRESS, LPS22HB_CTRL_REG1);
+    
+    /* Set default ODR */
+    tmp &= ~LPS22HB_ODR_MASK;
+    tmp |= (odr << 0x4) & LPS22HB_ODR_MASK; /*Set ODR*/
+    
+    /* Apply settings to CTRL_REG1 */
+    SENSOR_IO_Write(LPS22HB_I2C_ADDRESS, LPS22HB_CTRL_REG1, tmp);
+  }
 
-int STM32IotNodePressure::configure( )
-{
- if ( !samplePeriod )
-  samplePeriod = 1;
- float Value = 1000.0f / ( float ) samplePeriod;
- if ( ( ( PRESSURE_Drv_t* ) DrvContext.pVTable )->Set_ODR_Value( &DrvContext, Value ) != COMPONENT_OK )
-  return DEVICE_I2C_ERROR;
- if ( ( ( PRESSURE_Drv_t* ) DrvContext.pVTable )->Get_ODR( &DrvContext, &Value ) != COMPONENT_OK )
-  return DEVICE_I2C_ERROR;
- samplePeriod = 1000.0f / ( float ) Value;
- return DEVICE_OK;
-}
+  uint8_t getBestAdaptedODRValue(float& frequency){
+    uint8_t odr = 0; 
 
-/**
- * Poll to see if new data is available from the hardware. If so, update it.
- * n.b. it is not necessary to explicitly call this function to update data
- * (it normally happens in the background when the scheduler is idle), but a check is performed
- * if the user explicitly requests up to date data.
- *
- * @return The value on success, DEVICE_I2C_ERROR if the update fails.
- *
- */
+    if(frequency <= 5.f){
+      odr = 1; //frequency = 1Hz
+      frequency = 1.f;
+    }
+    else if (frequency <= 18.f){
+      odr = 2; //frequency = 10Hz
+      frequency = 10.f;
+    }
+    else if (frequency <= 38.f){
+      odr = 3; //frequency = 25Hz
+      frequency = 25.f;    
+    }
+    else if (frequency <= 63.f){
+      odr = 4; //frequency = 50Hz
+      frequency = 50.f;    
+    }
+    else if (frequency > 63.f){
+      odr = 5; //frequency = 75Hz
+      frequency = 75.f;    
+    }
+    else{
+      odr = 0; //one shot
+      frequency = 0;
+    }
+    return odr;
 
-int STM32IotNodePressure::readValue()
-{
- if ( !DrvContext.isInitialized )
- {
-   ( ( PRESSURE_Drv_t* ) DrvContext.pVTable )->Init( &DrvContext );
-  STM32IotNodePressure::configure();
-  ( ( PRESSURE_Drv_t* ) DrvContext.pVTable )->Sensor_Enable( &DrvContext );
- }
- float Data;
- if ( ( ( PRESSURE_Drv_t* ) DrvContext.pVTable )->Get_Press( &DrvContext, &Data ) == COMPONENT_OK )
-  return ( int ) ( Data );
- return DEVICE_I2C_ERROR;
-}
+  }
+
+  /**
+   * Configures the pressure range defined
+   * in this object. The nearest values are chosen to those defined
+   * that are supported by the hardware. The instance variables are then
+   * updated to reflect reality.
+   *
+   * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the pressure could not be configured.
+   *
+   */
+  int STM32IotNodePressure::configure( )
+  {
+    int ret = DEVICE_OK;
+
+    if(LPS22HB_P_Drv.ReadID(LPS22HB_I2C_ADDRESS) != LPS22HB_WHO_AM_I_VAL){
+      ret = DEVICE_I2C_ERROR;
+    }
+    else{  
+      psensor_drv->Init(LPS22HB_I2C_ADDRESS);
+    }
+    
+    if ( !samplePeriod )
+      samplePeriod = 1;
+
+    float frequency = 1000.0f / (float) samplePeriod;
+
+    uint8_t odr = getBestAdaptedODRValue(frequency); 
+    updateODR(odr);
+
+    samplePeriod = 1000.0f / frequency;
+
+    isInitialized = true;
+    return ret;
+  }
+
+  /**
+   * Poll to see if new data is available from the hardware. If so, update it.
+   * n.b. it is not necessary to explicitly call this function to update data
+   * (it normally happens in the background when the scheduler is idle), but a check is performed
+   * if the user explicitly requests up to date data.
+   *
+   * @return The value on success, DEVICE_I2C_ERROR if the update fails.
+   *
+   */
+  int STM32IotNodePressure::readValue()
+  {
+    if(!isInitialized)
+      configure();
+    return (int) psensor_drv->ReadPressure(LPS22HB_I2C_ADDRESS);
+  }
 
 }
